703d4967b0963b6a714d7b751a836713958dba1a
ilyzori <nik.bukreev.2002@mail.ru>
2022-05-08 18:24:03 +0700

    Labs

---
+++ lab_2/lab_2/RomanNumberException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+internal class RomanNumberException : Exception
+{
+    public RomanNumberException() : base() { }
+    public RomanNumberException(string message) : base(message) { }
+}
